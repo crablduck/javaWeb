88922dd0d4a39ab7b67d5df1b1d9830f9abb61fb
crablduck <asdfg756868947@qq.com>
2017-12-18 19:25:49 +0800

    struts2_namespace

---
+++ struts2_namespace/src/com/xjw/action/TeacherAction.java
@@ -0,0 +1,9 @@
+package com.xjw.action;
+
+public class TeacherAction {
+
+	public String addTeacher(){
+		System.out.println("TeacherAction.addTeacher()");
+		return null;
+	}
+}
---
+++ struts2_namespace/src/com/xjw/action/UserAction.java
@@ -0,0 +1,9 @@
+package com.xjw.action;
+
+public class UserAction {
+
+	public String addUser(){
+		System.out.println("user");
+		return null;
+	}
+}

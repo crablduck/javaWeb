7afc26376333be6bdfd43726a8ae28b621f392d7
crablduck <asdfg756868947@qq.com>
2017-12-18 19:50:39 +0800

    dynamic_method_invocation_more

---
+++ struts2_dynamic_more/src/com/xjw/action/UserAction.java
@@ -0,0 +1,26 @@
+package com.xjw.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class UserAction {
+
+	public String add(){
+		System.out.println("enclosing_type.enclosing_method()");
+		return Action.SUCCESS;
+	}
+	
+	public String delete(){
+		System.out.println("UserAction.delete()");
+		return Action.SUCCESS;
+	}
+	
+	public String update(){
+		System.out.println("UserAction.update()");
+		return Action.SUCCESS;
+	}
+	
+	public String query(){
+		System.out.println("UserAction.query()");
+		return Action.SUCCESS;
+	}
+}
